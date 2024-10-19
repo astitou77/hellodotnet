c00d64648a20d4c64e6268ae28a70390a59e58a7
Adnane Win10HP <astit77@gmail.com>
2024-10-19 10:16:31 -0400

    Added XML comments config to csproj

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace hellodotnet{
+    class Adnane{
+        /// This is an XML comment with 3 slashes
+        /// <summary>
+        /// This is a Test App to Say Hello
+        /// </summary>
+        /// <param name='args'>An array of string arguments from the command line</param>
+        /// <returns>
+        /// No value returned
+        /// </returns>
+        public static void Main(String[] args){
+            Console.WriteLine("What's yo name brah ?");
+            string str = Console.ReadLine();
+            Console.WriteLine("yo name iz: " + str);
+        }
+    }
+}
+
+
+
